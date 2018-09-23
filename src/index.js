43b16066788c664e01b3f0f01c6054399e4020bb
EvgenPrushk <ogmprushak@mail.ru>
2018-09-23 12:18:14 +0300

    Second task redy

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  /* need 3 lover for LoveTriangle */
+  if (preferences.length < 3) {
+  return 0;
+};
+
+var  counter = preferences.length - 2;
+var LoveTriangles = 0;
+/* block fariables */
+for(i = 0; i < counter; i++) {
+firstLoverCount = preferences[i];
+secondLoverCount = preferences[firstLoverCount-1];
+thidLoverCount = preferences[secondLoverCount-1];
+
+/* basic checker */
+if (firstLoverCount > i+1 && secondLoverCount > i+1 && thidLoverCount == i+1) {
+   LoveTriangles++;
+}
+
+}
+return LoveTriangles;
+
 };
